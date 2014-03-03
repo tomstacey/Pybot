286445c10ad6cf96149b9171b711b69c81c77c87
tomstacey <tomstacey@gmail.com>
2014-03-03 20:30:37 +0000

    Update pybot_raw_input.py

--- pybot_raw_input.py
+++ pybot_raw_input.py
@@ -19,15 +19,15 @@ print(killswitchstatus)
 ## functions - these functions actually make the hardware respond on a GPIO level
 
 def forward():
-	GPIO.output(11, GPIO.HIGH)
+    GPIO.output(11, GPIO.HIGH)
     GPIO.output(12, GPIO.HIGH)
     GPIO.output(13, GPIO.LOW)
-	GPIO.output(16, GPIO.HIGH)
+    GPIO.output(16, GPIO.HIGH)
     GPIO.output(18, GPIO.LOW)
     return
 
 def reverse():
-	GPIO.output(11, GPIO.HIGH)
+    GPIO.output(11, GPIO.HIGH)
     GPIO.output(12, GPIO.LOW)
     GPIO.output(13, GPIO.HIGH)
     GPIO.output(16, GPIO.LOW)
@@ -35,16 +35,16 @@ def reverse():
     return
     
 def left():
-	GPIO.output(11, GPIO.HIGH)
+    GPIO.output(11, GPIO.HIGH)
     GPIO.output(12, GPIO.LOW)
     GPIO.output(13, GPIO.HIGH)
     GPIO.output(16, GPIO.HIGH)
     GPIO.output(18, GPIO.LOW)
-	return
-	
-	
+    return
+
+
 def right():
-	GPIO.output(11, GPIO.HIGH)
+    GPIO.output(11, GPIO.HIGH)
     GPIO.output(12, GPIO.HIGH)
     GPIO.output(13, GPIO.LOW)
     GPIO.output(16, GPIO.LOW)
@@ -52,49 +52,43 @@ def right():
     return
     
 def stop():
-	GPIO.output(11, GPIO.LOW)
+    GPIO.output(11, GPIO.LOW)
     GPIO.output(12, GPIO.LOW)
     GPIO.output(13, GPIO.LOW)
     GPIO.output(16, GPIO.LOW)
     GPIO.output(18, GPIO.LOW)
     return
-	
+
 ##main loop
 
 while  1:
-	#key is a variable that is set to whatever key you type into the program
-	#type a letter and press enter to move PiBot
-	key = raw_input()
-	
-		## If you press w, set the motors to forward
-    	if key == 'w':
-        	forward();
+    #key is a variable that is set to whatever key you type into the program
+    #type a letter and press enter to move PiBot
+    key = raw_input()
+
+    ## If you press w, set the motors to forward
+    if key == 'w':
+        forward();
         ## and backward	
         
-        if key == 's':
-        	reverse();
+    if key == 's':
+        reverse();
         	
         ## left
-        if key == 'a':
-        	left();
+    if key == 'a':
+        left();
         	
-        ## right	
-        if key == 'd':
-        	right();
+    ## right	
+    if key == 'd':
+        right();
+
+    ## Z stops all motors
+    if key == 'z':
+    	stop();
         	
-        ## Z stops all motors
-        if key == 'z':
-    		stop();
-        	
-        ## q quits the program and the loop
-        if key == 'q':
-        	## This cleanup routine should be run somewhere to set the pins back
-        	GPIO.cleanup()
-        	break
+    ## q quits the program and the loop
+    if key == 'q':
+        ## This cleanup routine should be run somewhere to set the pins back
+        GPIO.cleanup()
+        break
         
-        
-    
-
-
-
-
