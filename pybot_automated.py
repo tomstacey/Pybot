c8a74c68e1a1c0a8d388dc6530602bcd772ec6cd
tomstacey <tomstacey@gmail.com>
2014-03-03 20:32:01 +0000

    Update with killswitch added

--- pybot_automated.py
+++ pybot_automated.py
@@ -2,6 +2,8 @@
 import RPi.GPIO as GPIO
 import time
 
+debug = true
+
 ##setup GPIO - this has to be done somewhere in the code
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT, initial=GPIO.HIGH)
@@ -12,45 +14,11 @@ GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(10, GPIO.IN)
 GPIO.setwarnings(False)
 
-## check killswitch and print
-killswitchstatus = GPIO.input(10)
-print(killswitchstatus)
 
-## functions - these functions actually make the hardware respond on a GPIO level
 
-def forward():
-    GPIO.output(11, GPIO.HIGH)
-    GPIO.output(12, GPIO.HIGH)
-    GPIO.output(13, GPIO.LOW)
-    GPIO.output(16, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
-    return
 
-def reverse():
-    GPIO.output(11, GPIO.HIGH)
-    GPIO.output(12, GPIO.LOW)
-    GPIO.output(13, GPIO.HIGH)
-    GPIO.output(16, GPIO.LOW)
-    GPIO.output(18, GPIO.HIGH)
-    return
-    
-def left():
-    GPIO.output(11, GPIO.HIGH)
-    GPIO.output(12, GPIO.LOW)
-    GPIO.output(13, GPIO.HIGH)
-    GPIO.output(16, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
-	return
-	
-	
-def right():
-    GPIO.output(11, GPIO.HIGH)
-    GPIO.output(12, GPIO.HIGH)
-    GPIO.output(13, GPIO.LOW)
-    GPIO.output(16, GPIO.LOW)
-    GPIO.output(18, GPIO.HIGH)
-    return
-    
+## Stop code
+
 def stop():
     GPIO.output(11, GPIO.LOW)
     GPIO.output(12, GPIO.LOW)
@@ -58,24 +26,74 @@ def stop():
     GPIO.output(16, GPIO.LOW)
     GPIO.output(18, GPIO.LOW)
     return
-	
-##main loop
 
-while  1:
-		#Go Forward for 1 sec
-		forward()
-		time.sleep(1)
-		
-		#Turn Right 90 degrees
-		left()
-		time.sleep(1)
-		
-		#add more movement here
-		break
-        
-GPIO.cleanup()        
+## check killswitch and print
+def killSwitch():
+    killswitchstatus = GPIO.input(10)
+    if killswitchstatus == 1:
+        print("Kill Switch Activated")
+        stop()
+    return
+
+## functions - these functions actually make the hardware respond on a GPIO level
+
+
+def forward(time):
+    killSwitch()
+    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(12, GPIO.HIGH)
+    GPIO.output(13, GPIO.LOW)
+    GPIO.output(16, GPIO.HIGH)
+    GPIO.output(18, GPIO.LOW)
+    killSwitch()
+    time.sleep(time)
+    return
+
+def reverse(time):
+    killSwitch()
+    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(12, GPIO.LOW)
+    GPIO.output(13, GPIO.HIGH)
+    GPIO.output(16, GPIO.LOW)
+    GPIO.output(18, GPIO.HIGH)
+    killSwitch()
+    time.sleep(time)
+    return
+    
+def left(time):
+    killSwitch()
+    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(12, GPIO.LOW)
+    GPIO.output(13, GPIO.HIGH)
+    GPIO.output(16, GPIO.HIGH)
+    GPIO.output(18, GPIO.LOW)
+    killSwitch()
+    time.sleep(time)
+    return
+
+
+def right(time):
+    killSwitch()
+    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(12, GPIO.HIGH)
+    GPIO.output(13, GPIO.LOW)
+    GPIO.output(16, GPIO.LOW)
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(time)
+    return
     
 
 
+##main loop
 
+while 1:
+    #Go Forward for 1 sec
+    forward(1)
 
+    #Turn Right 90 degrees
+    left(1)
+
+    #add more movement here
+    break
+        
+GPIO.cleanup()
